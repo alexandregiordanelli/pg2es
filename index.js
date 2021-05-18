64026260e14eff8d8f5c2d1d89972429f8ee94da
Alexandre <alexandre.giordanelli@gmail.com>
2021-05-18 14:48:07 -0300

    queue

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const amqp = require('amqplib');
+const { createClient }  = require('@supabase/supabase-js');
+
+const supabase = createClient("https://ggkolujyhxdkvzzlciri.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNzg0NDUzNiwiZXhwIjoxOTMzNDIwNTM2fQ.PlGQvJdggIOH8allQ3ARTnssK7kP0Rwat1UEtqbJIug")
 
 const main = async () => {
   const connection = await amqp.connect('amqp://rabbit')
   const channel = await connection.createChannel()
   const q = await channel.assertQueue("all", {deadLetterExchange: "amq.fanout"})
-  console.log("casa")
+  
+  supabase.from('Question').on('*', payload => {
+    channel.sendToQueue(q.queue, Buffer.from(JSON.stringify(payload)))
+  }).subscribe()
 }
 
 main()
